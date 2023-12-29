78109f09887cdb88226622637e7be0efe474f569
Artem Grechushkin <artem.grechushkin@mail.ru>
2023-12-29 23:16:17 +0000

    downloading and testing even more models, with claimed geospatial awareness (text)

--- embeddings/embeddings.py
+++ embeddings/embeddings.py
@@ -4,24 +4,51 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 model = SentenceTransformer('all-MiniLM-L6-v2')
 model2 = SentenceTransformer('sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2')
+# model = SentenceTransformer("dima-does-code/LaBSE-geonames-15K-MBML-5e-v2")
+# model = SentenceTransformer('chbwang/geo_multi-qa-mpnet-base-dot-v1')
+
+# model2 = SentenceTransformer("DataQueen/LAPSE_GEONAMES_RELOC")
 
 #Our sentences we like to encode
 sentences = [
     "Indian Ocean",
-    "South China Sea",
-    "East China Sea",
-    "Yellow Sea",
+    "China Sea",
+    "East Sea",
+    "Asian Sea",
+    "South Asia Sea",
+    "South China",
+    "Mediterranean Sea",
+    "Black Sea",
 ]
+
+# sentences = [
+#     "China",
+#     # put some cities in china
+#     "Beijing",
+#     "Shanghai",
+#     "Tianjin",
+
+#     # put some random non china cities
+#     "London",
+#     "Paris",
+#     "New York",
+#     "Tokyo",
+
+#     # put some japan cities
+
+#     "Osaka",
+#     "Kyoto",
+#     "Yokohama",
+# ]
 
 #Sentences are encoded by calling model.encode()
 embeddings = model.encode(sentences)
 embeddings2 = model2.encode(sentences)
 
-
-query_str = "South China Sea"
+# query_str = "China/Japan"
+# query_str = "Black Sea range or med sea"
 # query_str = "East Med/ Black Sea range"
 # Example query vector
-# query_vector = model.encode(["East Med/ Black Sea range"])[0]
 query_vector = model.encode([query_str])[0]
 query_vector2 = model2.encode([query_str])[0]
 
