d3579caa2968e8edc337a6e3591248d43c92dccb
Artem Grechushkin <artem.grechushkin@mail.ru>
2024-02-03 21:26:13 +0000

    geocoding with checking cache first and fallback to other fields if priority fields not found

--- geocoding.py
+++ geocoding.py
@@ -3,6 +3,8 @@ from geopy import distance
 from geopy.adapters import AioHTTPAdapter
 from setup import config, db_client
 import asyncio
+from db import GeocodedLocation, GeoJsonLocation, LocationForGeocoding
+import copy
 
 geolocator = Nominatim(user_agent="shipping_broker")
 
@@ -19,44 +21,88 @@ def init_async_geolocator_google():
         adapter_factory=AioHTTPAdapter
     )
 
-async def geocode_location(location: str) -> tuple | None:
+async def geocode_location_with_retry(location: LocationForGeocoding) -> GeocodedLocation | None:
+    # 1. Try geocode the PORT first. If it fails, try the SEA. If that fails, try the OCEAN.
+    # 2. If all fails, return None.
+    result = None
+
+    if location.port:
+        result = await geocode_location(location.port)
+        if result:
+            print("Location found using port.")
+            return result
+        else:
+            print("Location NOT found using port.")
+    
+    if location.sea:
+        result = await geocode_location(location.sea)
+        if result:
+            print("Location found using sea.")
+
+            # Idea is, if port exists but cannot be geocoded - we should still cache this location to the database, pointing to the same location as the sea.
+            if location.port:
+                print("Cache update for PORT, to point at the same location as SEA.")
+                # make copy of result and update the name to the port
+                dummy_location = copy.deepcopy(result)
+                dummy_location.name = location.port
+
+                await add_location_to_db(dummy_location)
+
+            
+            return result
+    
+    if location.ocean:
+        result = await geocode_location(location.ocean)
+        if result:
+            print("Location found using ocean.")
+            return result
+    
+    return result
+
+async def add_location_to_db(location: GeocodedLocation):
+    await known_locations.insert_one(location.model_dump())
+
+async def geocode_location(location: str) -> GeocodedLocation | None:
     """Converts a location string to a tuple of latitude and longitude"""
 
     result = None
 
     # Check if location is already in the database
-    known_location = await known_locations.find_one({"location": location})
+    known_location = await known_locations.find_one({"name": location})
 
     if known_location:
         print("Location found in database")
-        return known_location["latitude"], known_location["longitude"]
+        return GeocodedLocation(**known_location)
     else:
         print("Location not found in database")
 
-
     # Fallback to geocoding using the Google Maps API
     async with init_async_geolocator_google() as geolocator_google:
-        result = await geolocator_google.geocode(query = location) # type: ignore
-        if result:
+        response = await geolocator_google.geocode(query = location) # type: ignore
+        if response:
             print("Location found using Google Maps API")
-            # Save location to database
-            await known_locations.insert_one({
-                "location": location,
-                "latitude": result.latitude,
-                "longitude": result.longitude
-            })
-            print(result.raw)
-            return result.latitude, result.longitude
-    
+            result = GeocodedLocation(
+                name=location,
+                location=GeoJsonLocation(
+                    type="Point",
+                    coordinates=[response.longitude, response.latitude]
+                ),
+                address=response.address,
+                raw=response.raw
+            )
+
+            await add_location_to_db(result)
+
+            return result
+
     return result
 
 async def main():
-    loc1 = await geocode_location("Venezuela serpent edge")
-    loc2 = await geocode_location("ravenna")
-
-    # get distance in km between Odessa and Nemrut
-    dist = distance.distance(loc1, loc2).km
-    print(dist)
+    loc1 = await geocode_location_with_retry(LocationForGeocoding(
+        port="ECSA",
+        sea="East Coast South America",
+        ocean=""
+    ))
 
 if __name__ == "__main__":
     asyncio.run(main())
